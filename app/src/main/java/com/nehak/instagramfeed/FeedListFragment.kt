325a1c23a5666da4914eab5bf6623387492e66c9
Neha <62131967+NehaVerma993@users.noreply.github.com>
2021-01-09 02:40:05 +0530

    Added FeedListFragment, in which the feeds will show

---
+++ app/src/main/java/com/nehak/instagramfeed/FeedListFragment.kt
@@ -0,0 +1,12 @@
+package com.nehak.instagramfeed
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class FeedListFragment : Fragment(R.layout.fragment_feed_list) {
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
+}
--- app/src/main/java/com/nehak/instagramfeed/MainActivity.kt
+++ app/src/main/java/com/nehak/instagramfeed/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.nehak.instagramfeed
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.nehak.instagramfeed.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
