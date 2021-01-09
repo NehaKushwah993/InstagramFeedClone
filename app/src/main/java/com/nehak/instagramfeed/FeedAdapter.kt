4dd81d8cc1d43b784adfcfd6546da1aa116a5bf9
Neha <62131967+NehaVerma993@users.noreply.github.com>
2021-01-09 17:31:41 +0530

    Provided ViewHolder using Databinding class of "item_feed"

--- app/src/main/java/com/nehak/instagramfeed/FeedAdapter.kt
+++ app/src/main/java/com/nehak/instagramfeed/FeedAdapter.kt
@@ -8,7 +8,9 @@ import com.nehak.instagramfeed.databinding.ItemFeedBinding
 class FeedAdapter : RecyclerView.Adapter<FeedViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedViewHolder {
-        return super.createViewHolder(parent, viewType)
+        return FeedViewHolder(
+            ItemFeedBinding.inflate(LayoutInflater.from(parent.context))
+        );
     }
 
     override fun onBindViewHolder(holder: FeedViewHolder, position: Int) {
