07863bd1489d3e1192e4da5fd8712e2be32e476f
Neha <62131967+NehaVerma993@users.noreply.github.com>
2021-01-09 17:33:27 +0530

    Using "ListAdapter - provided by JetPack" instead of "RecyclerView.Adapter"

--- app/src/main/java/com/nehak/instagramfeed/FeedAdapter.kt
+++ app/src/main/java/com/nehak/instagramfeed/FeedAdapter.kt
@@ -2,10 +2,26 @@ package com.nehak.instagramfeed
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.nehak.instagramfeed.dataModels.FeedItem
 import com.nehak.instagramfeed.databinding.ItemFeedBinding
 
-class FeedAdapter : RecyclerView.Adapter<FeedViewHolder>() {
+class FeedAdapter : ListAdapter<FeedItem, FeedViewHolder>(DIFF_CALLBACK) {
+
+    companion object {
+        /** Mandatory implementation inorder to use "ListAdapter" - new JetPack component" **/
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<FeedItem>() {
+            override fun areItemsTheSame(oldItem: FeedItem, newItem: FeedItem): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: FeedItem, newItem: FeedItem): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedViewHolder {
         return FeedViewHolder(
