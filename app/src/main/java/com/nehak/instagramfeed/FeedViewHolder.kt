6b4aff50ad895701cd92b1d14b1def9f63514525
Neha <62131967+NehaVerma993@users.noreply.github.com>
2021-01-09 17:27:52 +0530

    Created ViewHolder class + binding connection of item_feed (recyclerView item)

---
+++ app/src/main/java/com/nehak/instagramfeed/FeedViewHolder.kt
@@ -0,0 +1,9 @@
+package com.nehak.instagramfeed
+
+import android.view.View
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.nehak.instagramfeed.databinding.ItemFeedBinding
+
+class FeedViewHolder(binding: ItemFeedBinding) : RecyclerView.ViewHolder(binding.root) {
+}
