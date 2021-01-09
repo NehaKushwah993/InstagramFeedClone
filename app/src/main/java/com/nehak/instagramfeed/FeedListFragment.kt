8f616720f1cb01013c8b6cbbb97b441015aefaf3
Neha <62131967+NehaVerma993@users.noreply.github.com>
2021-01-09 15:18:16 +0530

    Added MainFrament layout using data binding

--- app/src/main/java/com/nehak/instagramfeed/FeedListFragment.kt
+++ app/src/main/java/com/nehak/instagramfeed/FeedListFragment.kt
@@ -1,9 +1,27 @@
 package com.nehak.instagramfeed
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.nehak.instagramfeed.databinding.FragmentFeedListBinding
 
-class FeedListFragment : Fragment(R.layout.fragment_feed_list) {
+class FeedListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return DataBindingUtil.inflate<FragmentFeedListBinding>(
+            inflater,
+            R.layout.fragment_feed_list,
+            container,
+            false
+        ).root
+    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
