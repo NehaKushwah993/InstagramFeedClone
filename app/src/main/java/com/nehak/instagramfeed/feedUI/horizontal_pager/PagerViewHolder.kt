d594461e0f47573d46102c8c57a6fd24a2c94233
NehaK <neha.kushwah993@gmail.com>
2023-12-24 21:27:06 +0530

    Added video support for horizontal

---
+++ app/src/main/java/com/nehak/instagramfeed/feedUI/horizontal_pager/PagerViewHolder.kt
@@ -0,0 +1,21 @@
+package com.nehak.instagramfeed.feedUI.horizontal_pager
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.nehak.instagramfeed.databinding.ImageItemSingleBinding
+import com.nehak.instagramfeed.databinding.VideoItemSingleBinding
+
+/**
+ * Create By Neha Kushwah
+ */
+abstract class PagerViewHolder(root: View) : RecyclerView.ViewHolder(root)
+
+class ImageViewHolder(binding: ImageItemSingleBinding) : PagerViewHolder(binding.root) {
+    val imageView =
+        binding.imageView;
+}
+
+class VideoViewHolder(binding: VideoItemSingleBinding) : PagerViewHolder(binding.root) {
+    val videoThumbnail = binding.feedThumbnailView;
+    val customPlayerView = binding.feedPlayerView;
+}
